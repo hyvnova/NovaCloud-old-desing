99d253f8a29b8c14a9a1594ff22061fc3fd827d0
NoxxDev <70229868+NoxxDev@users.noreply.github.com>
2021-04-15 17:39:36 -0400

    add english alert vercion

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("La pagina aun sigue en desarollo \n Made By: NovaDev \n Gracias por visitar la pagina <3.")
+alert("La pagina aun sigue en desarollo \n Made By: NovaDev \n Gracias por visitar la pagina <3.\n\n Tha page still being in devlopment \n Made by: NovaDev \n Thanks for Coming <3.")
